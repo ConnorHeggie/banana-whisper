6cf9e8978296e9d070edcb3cb40cecf272e88315
Connor Heggie <connor_heggie@yahoo.com>
2022-12-15 18:45:46 -0800

    Changes to allow initial prompt and whisper large v2

--- app.py
+++ app.py
@@ -1,33 +1,37 @@
+import base64
+import os
+from io import BytesIO
+
 import torch
 import whisper
-import os
-import base64
-from io import BytesIO
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    model = whisper.load_model("base")
+    assert torch.cuda.is_available()
+    model = whisper.load_model("large-v2")
+
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
-    mp3BytesString = model_inputs.get('mp3BytesString', None)
+    mp3BytesString = model_inputs.get("mp3BytesString", None)
+    initial_prompt = model_inputs.get("initial_prompt", None)
+
     if mp3BytesString == None:
-        return {'message': "No input provided"}
-    
+        return {"message": "No input provided"}
+
     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
-    with open('input.mp3','wb') as file:
+    with open("input.mp3", "wb") as file:
         file.write(mp3Bytes.getbuffer())
-    
+
     # Run the model
-    result = model.transcribe("input.mp3")
-    output = {"text":result["text"]}
+    result = model.transcribe("input.mp3", initial_prompt=initial_prompt)
     os.remove("input.mp3")
     # Return the results as a dictionary
-    return output
+    return result
