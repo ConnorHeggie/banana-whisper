d6d0c0f497c83604eed59a71899671345c022713
Connor Heggie <connor_heggie@yahoo.com>
2022-12-15 19:11:28 -0800

    Change to large v2

--- download.py
+++ download.py
@@ -3,11 +3,13 @@
 
 # In this example: A Huggingface BERT model
 
-import whisper
 import torch
+import whisper
+
 
 def download_model():
-    model = whisper.load_model("base")
+    model = whisper.load_model("large-v2")
+
 
 if __name__ == "__main__":
     download_model()
